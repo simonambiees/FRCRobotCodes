51fc8432a875cbd8029e15805fc71d85ef0683aa
1115443660@qq.com <simon@192.168.213.1>
2018-01-27 14:39:11 +0800

    jx

--- src/org/usfirst/frc/team6394/robot/Robot.java
+++ src/org/usfirst/frc/team6394/robot/Robot.java
@@ -90,32 +90,3 @@ public class Robot extends IterativeRobot {
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package org.usfirst.frc.team6394.robot;
-
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Spark;
-import edu.wpi.first.wpilibj.drive.DifferentialDrive;
-
-/**
- * This is a demo program showing the use of the RobotDrive class, specifically
- * it contains the code necessary to operate a robot with tank drive.
- */
-public class Robot extends IterativeRobot {
-	private DifferentialDrive m_myRobot;
-	private Joystick m_leftStick;
-	private Joystick m_rightStick;
-
-	@Override
-	public void robotInit() {
-		m_myRobot = new DifferentialDrive(new Spark(0), new Spark(1));
-		m_leftStick = new Joystick(0);
-		m_rightStick = new Joystick(1);
-	}
-
-	@Override
-	public void teleopPeriodic() {
-		m_myRobot.tankDrive(m_leftStick.getY(), m_rightStick.getY());
->>>>>>> branch 'master' of https://github.com/simonambiees/FRCRobotCodes.git
-	}
-}
